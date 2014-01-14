232bacd37bc976e6830548b19b48510923477f6c
7h1b0 <wyrm1992@hotmail.fr>
2014-01-14 11:24:52 +0100

    Update tri.c

--- src/tri.c
+++ src/tri.c
@@ -7,11 +7,25 @@ void permute(struct Requete *x,struct Requete *y) {
 	*y = tmp;
 }
 
-int tri_bulle(struct Requete * t, int n) {
+int tri_bulle_horloge(struct Requete * t, int n) {
 	int num_balayage, i;
 	for (num_balayage = 1 ; num_balayage < n ; ++num_balayage) /* n-1 balayages */
 		for (i = 1 ; i < n ; ++i) /* balayage n°i : n-1 comparaisons */
 			if (t[i].horloge < t[i-1].horloge)  /* si deux elements ne sont pas dans l'ordre */
-				permute(&t[i], &t[i-1]); /* alors on les echange */
+					permute(&t[i], &t[i-1]); /* alors on les echange */
 	return 0;
+}
+
+int tri_bulle_site(struct Requete * t, int n) {
+	int num_balayage, i;
+	for (num_balayage = 1 ; num_balayage < n ; ++num_balayage) /* n-1 balayages */
+		for (i = 1 ; i < n ; ++i) /* balayage n°i : n-1 comparaisons */
+			if (t[i].site < t[i-1].site && t[i].horloge == t[i-1].horloge)  /* si deux elements ne sont pas dans l'ordre */
+					permute(&t[i], &t[i-1]); /* alors on les echange */
+	return 0;
+}
+
+int tri_bulle(struct Requete * t, int n) {
+	tri_bulle_horloge(t,n);
+	tri_bulle_site(t,n);
 }
